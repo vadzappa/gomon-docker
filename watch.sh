6292a9f6e2f632b8da63ac806962741e4762656f
Vadzim Zapolski <vadzim.zapolski@pipedrive.com>
2020-01-16 18:08:28 +0200

    modified watched to stay PID1 and orchestrate other processes and react on USR1

--- watch.sh
+++ watch.sh
@@ -5,39 +5,8 @@ if [ -n "${GO_NO_DEBUG}" ]; then
   IS_DEBUG='false'
 fi
 
-doRun() {
-  echo "Running..."
-  if [ "${IS_DEBUG}" = "true" ]; then
-    dlv debug --wd /app --listen=:40000 --output=/tmp/debug_bin --headless=true --api-version=2 --log &
-  else
-    (cd /app && go build -race -o /tmp/run_bin)
-    (cd /app && /tmp/run_bin &)
-  fi
-}
-
-shutDown() {
-  echo "Shutting down"
-
-  if [ "${IS_DEBUG}" = "true" ]; then
-    while [ -n "$(pidof dlv)" ] || [ -n "$(pidof debug_bin)" ]; do
-      kill -9 "$(pidof dlv)" >/dev/null 2>&1
-      kill -9 "$(pidof debug_bin)" >/dev/null 2>&1
-    done
-  else
-    while [ -n "$(pidof run_bin)" ]; do
-      kill -s TERM "$(pidof run_bin)" >/dev/null 2>&1
-    done
-  fi
-}
-
-trap "shutDown" INT TERM
-
-doRun
-
-rerunServer() {
-  shutDown
-  doRun
-}
+tail_pid=0
+notifier_pid=0
 
 lockBuild() {
   if [ -f /tmp/server.lock ]; then
@@ -50,14 +19,69 @@ unlockBuild() {
   rm -f /tmp/server.lock
 }
 
-inotifywait -e MODIFY -e DELETE -q -r -m /app 2>/dev/null |
-  while read -r path action file; do
-    ext="${file##*.}"
-    if [ "${ext}" != "go" ]; then
-      continue
-    fi
-    lockBuild
-    echo "${action}-ed ${file}"
-    rerunServer
-    unlockBuild
-  done
+doRun() {
+  echo "Starting up application..."
+  if [ "${IS_DEBUG}" = "true" ]; then
+    dlv debug --wd /app --listen=:40000 --output=/tmp/debug_bin --headless=true --api-version=2 --log &
+  else
+    (cd /app && go build -race -o /tmp/run_bin)
+    (cd /app && /tmp/run_bin &)
+  fi
+}
+
+stopApp() {
+  if [ "${IS_DEBUG}" = "true" ]; then
+    while [ -n "$(pidof dlv)" ] || [ -n "$(pidof debug_bin)" ]; do
+      kill -9 "$(pidof dlv)" >/dev/null 2>&1
+      kill -9 "$(pidof debug_bin)" >/dev/null 2>&1
+    done
+  else
+    while [ -n "$(pidof run_bin)" ]; do
+      kill -s TERM "$(pidof run_bin)" >/dev/null 2>&1
+    done
+  fi
+}
+
+shutDown() {
+  echo "Shutting down"
+
+  if [ $notifier_pid -ne 0 ]; then
+    kill -s 9 $notifier_pid
+  fi
+
+  stopApp
+
+  exit 0
+}
+
+restart() {
+  trap - USR1
+  echo "Restarting application..."
+  lockBuild
+
+  stopApp
+  doRun
+  unlockBuild
+
+  if [ $tail_pid -ne 0 ]; then
+    kill -s 9 $tail_pid
+  fi
+
+  trap 'restart' USR1
+
+  tail -f /dev/null &
+  tail_pid=$!
+  wait $tail_pid
+}
+
+trap 'restart' USR1
+trap 'shutDown' INT TERM
+
+doRun
+
+/inotifier.sh &
+notifier_pid=$!
+
+tail -f /dev/null &
+tail_pid=$!
+wait $tail_pid
